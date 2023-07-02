deebe487eb9f543129a27c1a98cf49dd5aeb42ad
guicoelhodev <gs.coelho@outlook.com>
2023-07-02 17:03:47 -0300

    feat: create Earth class to render earth model

---
+++ src/models/earth/EarthModel.ts
@@ -0,0 +1,25 @@
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+export class EarthModel {
+  private glftLoader = new GLTFLoader()
+  private scene
+  public earthScene?: GLTF
+
+  constructor(scene: THREE.Scene) {
+    this.scene = scene
+  }
+
+  load() {
+    this.glftLoader.load('../../assets/earth/scene.gltf', (gltfScene) => {
+      gltfScene.scene.position.x = -3.8
+      gltfScene.scene.position.y = 3.4
+      gltfScene.scene.position.z = -4.4
+      gltfScene.scene.rotation.x = 0.2
+
+      gltfScene.scene.scale.set(0.002, 0.002, 0.002)
+
+      this.scene.add(gltfScene.scene)
+      this.earthScene = gltfScene
+    })
+  }
+}
