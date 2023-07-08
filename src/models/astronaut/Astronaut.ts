e88716ba7282a73939af6c75ebd7da361dcad4ae
guicoelhodev <gs.coelho@outlook.com>
2023-07-08 18:34:41 -0300

    refactor: remove first animation function and create function to load all animations

--- src/models/astronaut/Astronaut.ts
+++ src/models/astronaut/Astronaut.ts
@@ -1,5 +1,6 @@
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import * as THREE from 'three'
+import { IAstronautAnimations } from '@/types/types'
 
 type IAnimation = {
   clip: THREE.AnimationClip
@@ -16,58 +17,49 @@ export class AstronautModel {
     this.scene = scene
   }
 
-  load() {
+  async load() {
     const loader = new FBXLoader()
-
     loader.setPath('../../assets/astronaut/')
 
-    loader.load('character.fbx', (fbx) => {
-      fbx.scale.setScalar(0.3)
-      fbx.traverse((c) => {
-        c.castShadow = true
-      })
+    const fbx = await loader.loadAsync('character.fbx')
 
-      fbx.position.y = -3.4
-      fbx.rotation.y = 3.2
-
-      this.scene.add(fbx)
-      this.mixer = new THREE.AnimationMixer(fbx)
-      this.animate(this.mixer)
-
-      //console.log(this.animations)
+    fbx.scale.setScalar(0.3)
+    fbx.traverse((c) => {
+      c.castShadow = true
     })
+
+    fbx.position.y = -3.4
+    fbx.rotation.y = 3
+
+    this.scene.add(fbx)
+    this.mixer = new THREE.AnimationMixer(fbx)
+
+    return fbx
   }
 
-  animate(mixer: THREE.AnimationMixer) {
+  async loadAllAnimations() {
     const animLoader = new FBXLoader()
     animLoader.setPath('../../assets/astronaut/')
+    const animations = []
 
-    animLoader.load('walking.fbx', (a) => this.Onload('walking', a, mixer))
-    animLoader.load('sitted.fbx', (a) => this.Onload('sitted', a, mixer))
-    animLoader.load('standingUp.fbx', (a) =>
-      this.Onload('standingUp', a, mixer)
-    )
+    const relativeAnimPaths = [
+      'breathing.fbx',
+      'sitted.fbx',
+      'standingUp.fbx',
+      'jump.fbx',
+    ]
 
-    const currentAnimation = this.animations[0]
-    if (currentAnimation) {
-      currentAnimation.action.play()
+    for await (let anim of relativeAnimPaths) {
+      let animFbx = await animLoader.loadAsync(anim)
+
+      let nameWithoutExtension = anim.split('.')[0]
+      animFbx = { ...animFbx, name: nameWithoutExtension } as THREE.Group
+
+      animations.push(animFbx)
     }
-  }
 
-  private Onload(
-    animName: string,
-    anim: THREE.Group,
-    mixer: THREE.AnimationMixer
-  ) {
-    const clip = anim.animations[0]
-    const action = mixer.clipAction(clip)
-
-    //action.play()
-
-    this.animations.push({
-      clip: clip,
-      action: action,
-      name: animName,
-    })
+    return {
+      animations,
+    }
   }
 }
