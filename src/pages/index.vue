1dd2d96ce20eb1901b3942a6e942a14c392910ae
guicoelhodev <gs.coelho@outlook.com>
2023-09-25 22:43:46 -0300

    feat: create base text to simulate initial experience

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
-<script lang="ts" setup>
+<script setup>
 import * as THREE from 'three'
 import { onMounted } from 'vue'
+import { Icon } from '@iconify/vue'
 
 // imports planets and astronaut
 
@@ -12,6 +13,8 @@ import { RocketModel } from '@/models/rocket/RocketModel'
 import { AstronautModel } from '@/models/astronaut/Astronaut'
 import { Galaxy } from '@/controls/Galaxy'
 import { MoveCharacter } from '@/controls/MoveCharacter'
+
+const componentWasMounted = ref(false)
 
 onMounted(async () => {
   const galaxy = new Galaxy()
@@ -45,8 +48,8 @@ onMounted(async () => {
     if (!planets) return
     if (planets.some((i) => i)) {
       planets.forEach((planet) => {
-        planet!.scene.rotation.y += 0.001
-        planet!.scene.rotation.x += 0.0001
+        planet.scene.rotation.y += 0.001
+        planet.scene.rotation.x += 0.0001
       })
     }
   }
@@ -56,7 +59,7 @@ onMounted(async () => {
   const clock = new THREE.Clock()
 
   const animate = () => {
-    requestAnimationFrame(animate as any)
+    requestAnimationFrame(animate)
     renderer.render(scene, camera)
     starshipModel.starsMesh.rotation.x += 0.0005
 
@@ -70,10 +73,43 @@ onMounted(async () => {
   }
 
   animate()
+
+  componentWasMounted.value = true
 })
 </script>
 
-<template></template>
+<template>
+  <div
+    :v-if="componentWasMounted"
+    class="absolute inset-0 flex items-center justify-center"
+  >
+    <TypeWritter
+      msg="Hi, I'm Guilherme Coelho"
+      :delayTime="2000"
+      classname="text-4xl"
+    />
+
+    <TypeWritter
+      msg="I'm work as front-end developer"
+      :delayTime="9000"
+      classname="text-4xl text-cyan-400 font-semibold"
+    />
+
+    <TypeWritter
+      msg="Welcome to my galaxy"
+      :delayTime="15000"
+      classname="text-6xl font-bold"
+    />
+
+    <article
+      id="space_container"
+      class="flex flex-col items-center justify-center gap-2"
+    >
+      <p class="text-white text-3xl">Press space to begin</p>
+      <Icon icon="tabler:space" color="white" width="68" height="68" />
+    </article>
+  </div>
+</template>
 
 <style>
 html {
