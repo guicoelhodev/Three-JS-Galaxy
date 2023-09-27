f3071ef468ed6e5c23cdfd371a6801e7c3645a69
Guilherme Santos Coelho <gs.coelho@outlook.com>
2023-09-27 18:31:50 -0300

    feat: create AxisZMessage component

---
+++ src/components/AxisZMessage/index.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { axisZEnum } from '@/enums/axisZEnum'
+import { useViewStore } from '@/store/useViewStore';
+
+const viewStore = useViewStore();
+
+watch(() => viewStore.axisZ, () => {
+  console.log(viewStore.axisZ);
+});
+
+const messageTypeCode = computed(() => {
+  const enumValues = Object.values(axisZEnum);
+
+  for (const value of enumValues){
+    if(viewStore.axisZ !== value) continue;
+    return value; 
+  }
+  return 0;
+})
+
+// watch(messageTypeCode, () => {
+//   // fadeIn('.info-1', true, 17);
+// })
+
+onMounted(() => {
+  //fadeIn('.info-2', false, 28);
+});
+</script>
+
+<template>
+
+  <!-- <section v-if="messageTypeCode === axisZEnum.moveText" class="info-1 centerDiv">
+    <p class="text-white text-3xl">Move text</p>
+  </section> -->
+
+  <!-- <section class="info-1 centerDiv">
+    <article
+      id="space_container"
+      class="flex flex-col items-center justify-center gap-2"
+    >
+      <p class="text-white text-3xl">Press space to begin</p>
+      <Icon icon="tabler:space" color="white" width="68" height="68" />
+    </article>
+  </section> -->
+
+  <section v-if="messageTypeCode === axisZEnum.moveText" class="info-2 centerDiv flex flex-col items-center gap-2">
+    <p class="text-white text-2xl font-semibold">
+      Use 'A' or 'D' to looking around
+    </p>
+    <p class="text-white text-2xl font-semibold">
+      Use 'W' or 'S' to move the astronaut
+    </p>
+  </section>
+
+  <section v-if="messageTypeCode === axisZEnum.webStack" class="info-2 centerDiv flex flex-col items-center gap-2">
+    <p class="text-white text-2xl font-semibold">
+      Thats my stacks
+    </p>
+  </section>
+
+  <section v-if="messageTypeCode === axisZEnum.projects" class="info-2 centerDiv flex flex-col items-center gap-2">
+    <p class="text-white text-2xl font-semibold">
+      Thats my projects
+    </p>
+  </section>
+
+</template>
+
+<style scoped>
+
+@keyframes fadeIn {
+  from { opacity: 0 }
+  to { opacity: 1 }
+};
+
+.centerDiv {
+  animation: fadeIn 1s;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
