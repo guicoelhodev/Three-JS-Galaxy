4cb8e2421bdfc869707a4a7613fb293a3ce6548e
Guilherme Santos Coelho <gs.coelho@outlook.com>
2023-10-06 11:52:19 -0300

    refactor: change link projects

--- src/data/carouselList.ts
+++ src/data/carouselList.ts
@@ -12,21 +12,21 @@ export const carouselList: ICarouselItem[] = [
     title: 'React Music',
     description: 'Adipisicing duis ad duis laborum ipsum duis labore voluptate ad ut cillum. Nulla dolore ullamco minim laboris et proident dolor aute. Qui aliquip pariatur adipisicing sit consequat fugiat incididunt aute dolor nostrud culpa incididunt. Dolor cillum sint nisi laborum in veniam nostrud ipsum aute dolore. Ut cupidatat veniam eiusmod mollit laboris elit ipsum est. Lorem pariatur officia ut fugiat tempor et ut et incididunt ad exercitation reprehenderit id aliqua. Aute qui occaecat sit id excepteur id cillum eu duis.',
     imagePath: 'https://github.com/guicoelhodev/ReactMusic/raw/main/github/readme_bg.png',
-    link: 'https://reactmusic.netlify.app/',
+    link: 'https://react-music-xi.vercel.app/',
     languages: ['react']
   },
   {
     title: 'Social Dev',
     description: 'Culpa esse reprehenderit officia fugiat voluptate mollit commodo ullamco tempor sint pariatur Lorem aute. Enim veniam nulla laborum nisi deserunt pariatur officia commodo occaecat duis. Aute duis tempor tempor consectetur ea Lorem ut enim consectetur fugiat sint qui. Consequat in anim do aliqua cillum et. Et laboris nostrud ex eu.',
     imagePath: 'https://github.com/guicoelhodev/social_dev/raw/main/github/img/homepage_preview.png',
-    link: 'https://social-dev.netlify.app/',
+    link: 'https://social-dev-sandy.vercel.app/',
     languages: ['react']
   },
   {
     title: 'Discord clone',
     description: 'Occaecat anim magna do laboris cupidatat. In quis laborum id commodo do tempor laborum dolor elit id dolore nisi. Est aliquip et consequat consectetur cupidatat officia ad id anim commodo.',
     imagePath: 'https://github.com/guicoelhodev/discord-clone/raw/master/github/home_view.png',
-    link: 'https://discord-clone.netlify.app/',
+    link: 'https://discord-clone-guicoelhodev.vercel.app/',
     languages: ['nuxt', 'tailwindcss']
   }
 ]
