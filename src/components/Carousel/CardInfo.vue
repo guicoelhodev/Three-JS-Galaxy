568da669256637679b9eb6fa81bd0c7ee0b86e78
Guilherme Santos Coelho <gs.coelho@outlook.com>
2023-10-06 11:27:09 -0300

    refactor: define properties

--- src/components/Carousel/CardInfo.vue
+++ src/components/Carousel/CardInfo.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
+import { ICarouselItem } from '@/types/types';
 import { Icon } from '@iconify/vue'
 import gsap from 'gsap'
 
-const { toggleModal } = defineProps({
-  toggleModal: {
-     type: Function,
-     required: true
-  }
-});
+type IProps = {
+  toggleModal: Function
+  info: ICarouselItem | null
+}
+const { toggleModal, info  } = defineProps<IProps>()
 
 const showContent = ref(false)
 
@@ -22,8 +22,8 @@ onMounted(() => {
         borderRadius: '8px',
         height: '600px',
         width: '600px',
-        ease: 'power1.in',
-        duration: 0.5,
+        ease: 'power2.in',
+        duration: 0.4,
         onComplete: () => {
           showContent.value = true
         },
@@ -31,6 +31,8 @@ onMounted(() => {
     },
   })
 })
+
+console.log(info)
 </script>
 
 <template>
@@ -38,9 +40,9 @@ onMounted(() => {
     style="pointer-events: visible;"
     class="z-30 container_item bg-zinc-50 w-0 h-0 rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
   >
-    <div v-if="showContent" class="card-content h-full max-h-[500px]">
+    <div v-if="showContent && info" class="card-content h-full max-h-[500px]">
       <header class="flex justify-between gap-4 p-4">
-        <h3 class="font-bold text-3xl text-blue-400">Pokedex-v3 project</h3>
+        <h3 class="font-bold text-3xl text-blue-400">{{ info.title }} </h3>
           <button class="cursor-pointer mr-1" v-on:click="() => toggleModal()">
             <Icon icon="ooui:close" height="28" color="gray" />
           </button>
@@ -50,17 +52,7 @@ onMounted(() => {
         <article class="w-full p-4 flex flex-col gap-4">
           <h4 class="subtitle">Description</h4>
 
-          <p class="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-            qui beatae harum iure vitae cupiditate blanditiis facilis mollitia
-            laborum doloribus, alias reiciendis molestias voluptatibus,
-            molestiae veniam repellendus et libero iusto.
-          </p>
-
-          <p class="text">
-            Lorem ipsum dolor sit deserunt adipisci dolorum ipsa amet vero
-            consequatur, fuga dignissimos quibusdam similique!
-          </p>
+          <p class="text">{{ info.description }}</p>
 
           <aside>
             <a
@@ -69,7 +61,7 @@ onMounted(() => {
             >
               <img
                 class="w-[340px] m-auto object-cover aspect-video rounded-md"
-                src="https://github.com/guicoelhodev/Pokedex-v3/raw/main/github/homepage_2.png"
+                :src="info.imagePath"
               />
             </a>
           </aside>
